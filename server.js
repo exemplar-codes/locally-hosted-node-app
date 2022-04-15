d0dd56ee1066efca43e910fa16109666d223d25d
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-04-16 00:39:31 +0530

    Send webpages

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello").status(200);
-  res.end();
+  res.sendFile("./public/index.html", { root: __dirname });
+  res.status(200);
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
